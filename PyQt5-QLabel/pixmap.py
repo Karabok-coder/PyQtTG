ecb5ba36d4e8908a7c045f38735439d919bee786
Karabok <71427586+Karabok-coder@users.noreply.github.com>
2022-07-05 15:47:59 +0300

    Create pixmap.py

---
+++ PyQt5-QLabel/pixmap.py
@@ -0,0 +1,32 @@
+from PyQt5.QtWidgets import QMainWindow, QLabel, QWidget, QGridLayout
+from PyQt5.Qt import QApplication
+from PyQt5 import QtGui
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+
+        cental_widget = QWidget(self)
+        grid = QGridLayout()
+
+        cental_widget.setLayout(grid)
+        self.setCentralWidget(cental_widget)
+
+        label = QLabel(self)
+
+        pixmap = QtGui.QPixmap('1.png')
+        label.setPixmap(pixmap)
+
+        self.resize(pixmap.width(),pixmap.height())
+
+        grid.addWidget(label, 0, 0)
+
+
+if __name__ == "__main__":
+    import sys
+
+    app = QApplication([]) # QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    
+    sys.exit(app.exec_())
