cbae8961ab80c8124d8357c1504753059d02740d
Karabok <71427586+Karabok-coder@users.noreply.github.com>
2022-07-05 15:20:18 +0300

    Create selectedText.py

---
+++ PyQt5-QLabel/selectedText.py
@@ -0,0 +1,36 @@
+from PyQt5.QtWidgets import QMainWindow, QLabel, QWidget, QGridLayout, QPushButton
+from PyQt5.Qt import QApplication
+from PyQt5 import QtCore
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+
+        cental_widget = QWidget(self)
+        grid = QGridLayout()
+
+        cental_widget.setLayout(grid)
+        self.setCentralWidget(cental_widget)
+
+        self.label = QLabel('Мой первый виджет')
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setTextInteractionFlags(QtCore.Qt.TextInteractionFlag.TextSelectableByMouse)
+
+        btn = QPushButton('Изменить Alignment')
+        btn.clicked.connect(self.btn_alig)
+        
+        grid.addWidget(self.label, 0, 0)
+        grid.addWidget(btn, 1, 0)
+
+    def btn_alig(self):
+        print(self.label.selectedText())
+
+if __name__ == "__main__":
+    import sys
+
+    app = QApplication([]) # QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    
+    sys.exit(app.exec_())
+
